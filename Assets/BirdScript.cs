bd647338786b93fb1fcb0ff1f55641fb8b1ddedf
JoJo777890 <JoJo777890@gmail.com>
2023-11-20 18:19:52 +0800

    Add files via upload
    2023-11-20 (Only Unity Assets) Step 3 - Spawning Objects

---
+++ Assets/BirdScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody;
+    public int flapStrength = 10;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            myRigidbody.velocity = Vector2.up * flapStrength;
+        }
+    }
+}
---
+++ Assets/PipeMoveScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeMoveScript : MonoBehaviour
+{
+    public float moveSpeed = 7.5f;
+    public float deadZone = -40;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+
+        if (transform.position.x < -40)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
